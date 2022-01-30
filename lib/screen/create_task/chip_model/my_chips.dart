253e8105aab12528516340b682cc2b5dcd91eccc
Umarbek_Saidov <umarbeksaidov2003gmail.com>
2022-01-30 22:23:31 +0500

    team3 create task ui started

---
+++ lib/screen/create_task/chip_model/my_chips.dart
@@ -0,0 +1,21 @@
+class MyTopic {
+  String title;
+  bool isSelected;
+
+  @override
+  String toString() {
+    return 'MyTopic{title: $title, isSelected: $isSelected}';
+  }
+
+  MyTopic(this.title, {this.isSelected = false});
+
+  static List<MyTopic> topics = [
+    MyTopic('Design'),
+    MyTopic('Meeting'),
+    MyTopic('Coding'),
+    MyTopic('Meeting'),
+    MyTopic('BDE'),
+    MyTopic('Testing'),
+    MyTopic('Quic call'),
+  ];
+}
